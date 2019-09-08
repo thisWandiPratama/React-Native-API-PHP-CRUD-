093a5a72c4982bc31534a522c06560399362a7f7
thisWandiPratama <wandi99pratama@gmail.com>
2019-09-09 04:07:30 +0700

    Upload API PHP CRUD For React Native

---
+++ DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+$DatabaseName = "id6896565_datasiswa";
+
+//Define your database username here.
+$HostUser = "id6896565_datasiswa";
+
+//Define your database password here.
+$HostPass = "datasiswa";
+
+?>
